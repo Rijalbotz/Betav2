97f0f68f98ffd09735a72a02560be26b82231469
Tokusat <98427364+ERLANRAHMAT@users.noreply.github.com>
2022-02-22 12:23:16 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -722,6 +722,56 @@ Berapapun donasi kalian akan sangat berarti 👍
 
 Thanks!
 
+Tidak hanya itu kami juga menyediakan jasa pembuatan bot dan sewa bot lho:)
+
+*OPEN PEMBUATAN BOT WHATSAPP*
+
+*│• NOTE:WAJIB ADA AKUN*
+*│• GITHUB&HEROKU*
+*│• HARGA?20K*
+*╰────*
+*╭─ [ KELEBIHAN BOT ]*
+*│• FAST RESPON*
+*│• MENU GG*
+*│• ANTILINK, WELCOME ON,*   
+*│•DLL*
+*╰────*
+*╭─[ KEUNTUNGAN ]*
+*│• ANTI LINK ( AUTO KICK )*
+*│• WELCOME OTOMATIS*
+*│• BUAT STIKER*
+*│• PERINTAH ADMIN*
+*│• DLL, FITUR GG🥵*
+*╰────*
+
+_*📍 MINAT?*_
+_*CHAT :*085803583481(Lann)
+ ╭─ [ 🇸 🇪 🇼 🇦  🇧 🇴 🇹  ]
+╭─────────
+│ *❏ >12k : PERMANEN*
+    *❏ >5k:1 minggu*
+    *❏ >7k:1bulan*
+     *❏ Mau Test dulu boleh 1 jam*
+╰─────────
+*SISTEM : BOT MASUK GROUP>TF>DONE*
+╭─────────
+│ *❏<> BOT ON 24JAM*
+│ *❏<> ANTILINK*
+│ *❏<> WELCOME IMAGE*
+│ *❏<> TUTUP/BUKA GC*
+│ *❏<>  KICK ORANG*
+ |  *❏<> SHARE LINK AUTO KICK*
+ |  *❏<> DAN RATUSAN FITUR LAINNYA*
+╰─────────
+*⏰BOT ON 24JAM⏰*
+*🚀BOT ON 24JAM🚀*
+*📛ON TANPA WIFI/KUOTA JADI ON SELALU TANPA PUTUS KECUALI KENA KENON📛*
+
+*ORDER?WA*
+Wa.me//6285803583481?text=Mau+sewa+bott
+
+
+
 Contact person Owner:
 wa.me/${ownernomer}
 
